179f63f3d0f1ffbe9e6149061a3091ad0ad1f967
jdicorpo <65524726+jdicorpo@users.noreply.github.com>
2025-03-29 09:52:15 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,32 +16,35 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  assign uo_out = ui_in;
+    
   // Direct inputs
-  wire loopback = ui_in[0];
-  reg [15:0] counter;
-  reg [7:0] outport;
-  reg [7:0] biport;
-  reg [7:0] bidir;
+  // wire loopback = ui_in[0];
+  // reg [15:0] counter;
+  // reg [7:0] outport;
+  // reg [7:0] biport;
+  // reg [7:0] bidir;
   
-  assign uo_out = outport;
-  assign uio_out = biport;
-  assign uio_oe = bidir;
-  always @(posedge clk) begin
-    if (~rst_n) begin
-      counter <= 0;
-      outport <= 0;
-      biport <= 0;
-    end else begin
-      counter <= counter + 1;
-      if (loopback) begin
-        outport <= uio_in;
-        biport <= 0;
-        bidir <= 0;
-      end else begin
-        outport <= counter[7:0];
-        biport <= counter[15:8];
-        bidir <= 8'hff;
-      end
-    end
-  end
+  // assign uo_out = outport;
+  // assign uio_out = biport;
+  // assign uio_oe = bidir;
+  // always @(posedge clk) begin
+  //   if (~rst_n) begin
+  //     counter <= 0;
+  //     outport <= 0;
+  //     biport <= 0;
+  //   end else begin
+  //     counter <= counter + 1;
+  //     if (loopback) begin
+  //       outport <= uio_in;
+  //       biport <= 0;
+  //       bidir <= 0;
+  //     end else begin
+  //       outport <= counter[7:0];
+  //       biport <= counter[15:8];
+  //       bidir <= 8'hff;
+  //     end
+  //   end
+  // end
+    
 endmodule
